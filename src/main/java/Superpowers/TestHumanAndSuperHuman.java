8e23ff2044ce010a5a3e286dd31f1173b0966f6a
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-14 18:57:49 -0500

    started testing the methods

---
+++ src/main/java/Superpowers/TestHumanAndSuperHuman.java
@@ -0,0 +1,10 @@
+package Superpowers;
+
+public class TestHumanAndSuperHuman {
+    public static void main(String [] args) {
+        Human bob = new Human();
+        Human tom = new Human();
+
+        bob.getName();
+    }
+}
