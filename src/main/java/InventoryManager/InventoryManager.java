fc291a80f78fd5304d4dbb0577d42042cf7eaebe
Juliana Paul <paul.juliana15@gmail.com>
2020-07-14 13:54:19 -0400

    Initial commit

--- src/main/java/InventoryManager/InventoryManager.java
+++ src/main/java/InventoryManager/InventoryManager.java
@@ -1,5 +1,7 @@
 package InventoryManager;
 
+import java.util.ArrayList;
+
 /**
  * Finish the InventoryManager Class in the InventoryManager Package,
  * which manages an inventory of products. Create a product class which has a price,
@@ -9,4 +11,23 @@ package InventoryManager;
  * Note: Inventory and InventoryManager should not be the same class!!!
  */
 public class InventoryManager {
+
+    ArrayList<Inventory> location = new ArrayList<Inventory>();
+
+    public int addItems(Product id, Inventory quantity){
+
+        return 0;
+    }
+
+    public int addLocation(){
+        return 0;
+    }
+
+    public int removeItem(pr){
+        return 0;
+    }
+
+    public int re
+
+
 }
