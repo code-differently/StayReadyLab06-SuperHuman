26108c919ac39a9ecf798cb6222e9ddef5444c08
Junior Mitchell <spoony@MacBook-Pro-2.local>
2020-07-14 18:43:34 -0400

    Human Class and Human Test finished w/ Kaveesha

--- src/main/java/Superpowers/Human.java
+++ src/main/java/Superpowers/Human.java
@@ -1,5 +1,5 @@
 package Superpowers;
-
+import java.time.LocalDate;
 /**
  * Complete the 'Human' class in the Superpowers Package that has fields for: name, age, gender,
  * occupation, and address. Also create methods for retreiving and outputing this data to screen.
@@ -7,4 +7,44 @@ package Superpowers;
  * hero name, super ability. As before, create methods for retrieving field data and printing to screen.
  */
 public class Human {
+    private int id;
+    private String name;
+    private LocalDate dob;
+    private String gender;
+    private String occupation;
+    public Human() {
+        id = 0;
+        name = "";
+        dob = LocalDate.now();
+        gender = "";
+        occupation = "";
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public LocalDate getLocalDate() {
+        return dob;
+    }
+    public void setLocalDate(LocalDate date) {
+        dob = date;
+    }
+    public String getGender() {
+        return gender;
+    }
+    public void setGender(String gender){
+        this.gender = gender;
+    }
+    public String getOccupation() {
+        return occupation;
+    }
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+    // @Override
+    // public String toString() {
+    //     return id + " " + name + " " + dob + " " + gender + " " + occupation;
+    // }
 }
