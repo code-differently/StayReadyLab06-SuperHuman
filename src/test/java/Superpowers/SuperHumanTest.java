959d44310738e07a567b2f5bdb4d62e3e359fd11
Joshua Johnson <jj17@iu.edu>
2020-07-14 21:47:50 -0400

    superhuman

--- src/test/java/Superpowers/SuperHumanTest.java
+++ src/test/java/Superpowers/SuperHumanTest.java
@@ -1,16 +1,21 @@
 package Superpowers;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.time.LocalDate;
 
 public class SuperHumanTest {
 
-    SuperHuman bad_thang = new SuperHuman("Cupcake", 100, "Female", "Cook", LocalDate.of(2000,3,15);
+    SuperHuman bad_thang = new SuperHuman("Cupcake", 100, "Female",
+            "Cook", LocalDate.of(2000,3,15), "Frost Kween",
+            "Frost Shot", SuperHuman.Affiliation.NEUTRAL,
+            "Your dessert served cold!", "Froooosh!");
+
     @Test
-    public void getHeroName() {
-        String expected = "Cupcake";
+    public void getHeroAlias() {
+        String expected = "Frost Kween";
         String actual = bad_thang.getAlias();
 
         Assert.assertEquals(expected, actual);
@@ -18,16 +23,16 @@ public class SuperHumanTest {
 
     @Test
     public void setHeroName() {
-        String expected = "Kupcake";
         bad_thang.setAlias("Kupcake");
         String actual = bad_thang.getAlias();
+        String expected = "Kupcake";
 
         Assert.assertEquals(expected, actual);
     }
 
     @Test
     public void getPower() {
-        String expected = "Quirk: Frost Shots";
+        String expected = "Frost Shot";
         String actual = bad_thang.getPower();
 
         Assert.assertEquals(expected, actual);
@@ -35,9 +40,9 @@ public class SuperHumanTest {
 
     @Test
     public void setPower() {
-        String expected = "BYAKUGAN";
         bad_thang.setPower("BYAKUGAN");
         String actual = bad_thang.getPower();
+        String expected = "BYAKUGAN";
 
         Assert.assertEquals(expected, actual);
     }
@@ -52,6 +57,7 @@ public class SuperHumanTest {
 
     @Test
     public void setAffiliations() {
+        ;
         SuperHuman.Affiliation expected = SuperHuman.Affiliation.GOOD;
         bad_thang.setAffiliations(SuperHuman.Affiliation.GOOD);
         SuperHuman.Affiliation actual = bad_thang.getAffiliations();
