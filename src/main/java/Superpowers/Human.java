0630ebbade16488a3deac93f6a12c9478e6c854e
Dominique <cyanthecomputerscientist@gmail.com>
2020-07-14 20:18:59 -0400

    Created human and super human class

--- src/main/java/Superpowers/Human.java
+++ src/main/java/Superpowers/Human.java
@@ -1,5 +1,8 @@
 package Superpowers;
 
+import java.time.LocalDate;
+
+
 /**
  * Complete the 'Human' class in the Superpowers Package that has fields for: name, age, gender,
  * occupation, and address. Also create methods for retreiving and outputing this data to screen.
@@ -7,4 +10,78 @@ package Superpowers;
  * hero name, super ability. As before, create methods for retrieving field data and printing to screen.
  */
 public class Human {
+
+    private int id;
+    private LocalDate dob;
+    private String name; 
+    private String gender; 
+    private String occupation;
+
+    Human()
+    {
+        this.id = 1;
+        this.dob  = LocalDate.of(2000, 1, 1);
+        this.name = "human";
+        this.gender = "Female";
+        this.occupation = "some job";
+    }
+    Human(int id, LocalDate dob, String name, String gender,String occupation)
+    {
+        this.id = id;
+        this.dob = dob;
+        this.name = name;
+        this.gender = gender;
+        this.occupation = occupation;
+    }
+
+    public int getID()
+    {
+        return this.id;
+    }
+
+    public int getAge()
+    {
+        LocalDate Year = LocalDate.now();
+        int thisYear = Year.getYear();
+        int birthYear = this.dob.getYear();
+        return thisYear - birthYear;
+    }
+
+    public LocalDate getDob()
+    {
+        return this.dob;
+    }
+    public String getName()
+    {
+        return this.name;
+    }
+    public String getGender()
+    {
+        return this.gender;
+    }
+    public String getOccupation()
+    {
+        return this.occupation;
+    }
+    public void setName(String n)
+    {
+        this.name= n;
+    }
+    public void setGender(String gender)
+    {
+        this.gender = gender;
+    }
+    public void setOccupation(String occupation)
+    {
+        this.occupation = occupation;
+    }
+    public void setDob(LocalDate dob)
+    {
+        this.dob = dob;
+    }
+   
+    
+   
+    
+    
 }
---
+++ src/main/java/Superpowers/Superhuman.java
@@ -0,0 +1,79 @@
+package Superpowers;
+
+enum Afilliation
+{
+    Good,Bad,Neutral;
+}
+public class Superhuman extends Human {
+    
+    private String alias;
+    private String ability; 
+    private Afilliation afilliation;
+    private String catchPhrase;
+    private String power; 
+
+    Superhuman()
+    {
+        this.alias = "My alias";
+        this.ability = "My ability";
+        this.afilliation = Afilliation.Good;
+        this.catchPhrase = "catch Phrase";
+        this.power = "power";
+
+    }
+    Superhuman(String ali, String abi, Afilliation afill, String cp, String pow)
+    {
+        this.alias= ali;
+        this.ability = abi; 
+        this.afilliation = afill;
+        this.catchPhrase=cp;
+        this.power = pow;
+    }
+
+    public String getalias()
+    {
+      return this.alias;
+    }
+    public void setalias(String ali)
+    {
+        this.alias = ali;
+    }
+    public String getability()
+    {
+      return this.ability;
+
+    }
+    public void setability(String abi)
+    {
+        this.ability = abi;
+    }
+    public Afilliation getafiliation()
+    {
+      return this.afilliation;
+
+    }
+    public void setafiliation(Afilliation afill)
+    {
+        this.afilliation = afill;
+    }
+    public String catchPhrase()
+    {
+        return this.catchPhrase;
+    }
+    public void setCatchPhrase(String cp)
+    {
+        this.catchPhrase = cp;
+    }
+    public String usePower()
+    {
+        return this.catchPhrase + this.power;
+    }
+    public void setPowers(String pow)
+    {
+        this.power = pow; 
+    }
+    public String getPowers()
+    {
+        return this.power;
+    }
+}
