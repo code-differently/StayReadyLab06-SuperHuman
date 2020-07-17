5043affbe119f5a1b3fb8c24112a9bceae4c66de
CourtneeL <48140227+CourtneeL@users.noreply.github.com>
2020-07-17 15:13:23 -0400

    Lab 06 done

--- src/main/java/Superpowers/Human.java
+++ src/main/java/Superpowers/Human.java
@@ -6,24 +6,24 @@ import java.time.LocalDate;
  * Complete the 'Human' class in the Superpowers Package that has fields for: name, age, gender,
  * occupation, and address. Also create methods for retreiving and outputing this data to screen.
  * Then create a SuperHuman class and UNIT TEST that subclasses the first with fields for good or bad,
- * hero name, super ability. As before, create methods for retrieving field data and printing to screen.
+ * hero name, superability. As before, create methods for retrieving field data and printing to screen.
  */
 public class Human 
 {
     private String name;
     private int age;
-    private LocalDate DOB;
+    private LocalDate dob;
     private String gender;
     private String occupation;
     private final int id;
 
-    public Human(String name,int id,String gender,String occupation, LocalDate DOB)
+    public Human(String name,int id,String gender,String occupation, LocalDate dob)
     {
-        this.name=name;
-        this.id=id;
-        this.gender=gender;
-        this.DOB=DOB;
-        this.occupation=occupation;
+        this.name = name;
+        this.id = id;
+        this.gender = gender;
+        this.dob = dob;
+        this.occupation = occupation;
     }
 
     public Human()
@@ -58,12 +58,12 @@ public class Human
 
     public LocalDate getDOB()
     {
-        return DOB;
+        return dob;
     }
 
     public void setDOB(LocalDate date)
     {
-        this.DOB = date;
+        this.dob = date;
     }
 
     public String getGender()
