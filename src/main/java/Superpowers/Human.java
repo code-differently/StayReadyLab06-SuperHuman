e40a06764e08ccdb62ffb29c69e8edc13c18d779
Kaiyen Thompson <kthompson6300@gmail.com>
2020-07-17 16:24:54 -0400

    ready for review

--- src/main/java/Superpowers/Human.java
+++ src/main/java/Superpowers/Human.java
@@ -1,5 +1,7 @@
 package Superpowers;
 
+import java.time.LocalDate;
+
 /**
  * Complete the 'Human' class in the Superpowers Package that has fields for: name, age, gender,
  * occupation, and address. Also create methods for retreiving and outputing this data to screen.
@@ -7,4 +9,18 @@ package Superpowers;
  * hero name, super ability. As before, create methods for retrieving field data and printing to screen.
  */
 public class Human {
+    private String name;
+    private int id;
+    private String gender;
+    private String occupation;
+    private LocalDate dob;
+
+    public Human(){
+
+    }
+    public Human(String name, int id, String gender, String occupation, LocalDate don){
+        this.name=name;
+        this.id=id;
+
+    }
 }
